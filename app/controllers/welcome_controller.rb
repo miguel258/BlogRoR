9ac83a8ff608f8b7a83a358c38c73535b3544f81
Miguel <mra634463@gmail.com>
2016-05-10 18:46:41 -0500

    Primer avance,conociendo MVC

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,5 @@
+class WelcomeController < ApplicationController #Esta clase hereda de 
+												#ApplicationController
+  def index
+  end
+end
---
+++ app/views/users/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @user, :id, :first_name, :last_name, :telephone, :created_at, :updated_at
